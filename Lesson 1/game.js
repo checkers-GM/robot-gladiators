56d8784a33cf28d8e057819f325c148eba285846
Nathan <nathaniel.mccaw@gmail.com>
2020-12-16 15:35:00 -0500

    start and end game features

--- Lesson 1/game.js
+++ Lesson 1/game.js
@@ -1,22 +1,28 @@
+// var playerName = 'Clank McKrank';
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-var enemyNames = ['Roborto', 'Amy Android', 'Robo Trumble'];
+console.log(playerName);
+console.log(playerHealth, playerAttack, playerMoney);
+console.log("You can put regular sentences in these.");
+
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+
+console.log(enemyNames);
+
+
+
+
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-console.log(enemyNames);
-console.log(enemyNames.length);
-console.log(enemyNames[0]);
-console.log(enemyNames[3]);
-
-// fight function (now with parameter for enemy's name)
+// fight function
 var fight = function(enemyName) {
     while (playerHealth > 0 && enemyHealth > 0) {
-        // ask player if they'd like to fight or run
-        var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+        // ask player if they'd liked to fight or run
+        var promptFight = window.prompt('Would you like FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
         // if player picks "skip" confirm and then stop the loop
         if (promptFight === "skip" || promptFight === "SKIP") {
@@ -28,7 +34,7 @@ var fight = function(enemyName) {
                 window.alert(playerName + ' has decided to skip this fight. Goodbye!');
                 // subtract money from playerMoney for skipping
                 playerMoney = playerMoney - 10;
-
+                console.log("playerMoney", playerMoney)
                 break;
             }
         }
@@ -69,50 +75,20 @@ var fight = function(enemyName) {
     }
 };
 
-// fight each enemy-robot by looping over them and fighting them one at a time
-var startGame = function() {
-    for (var i = 0; i < enemyNames.length; i++) {
-        // if player is still alive, keep fighting
-        if (playerHealth > 0) {
-            // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
-            window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
-
-            // pick new enemy to fight based on the index of the enemyNames array
-            var pickedEnemyName = enemyNames[i];
-
-            // reset enemyHealth before starting new fight
-            // enemyHealth = 50;
-
-            // use debugger to pause script from running and check what's going on at that moment in the code
-            debugger;
-
-            // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
-            fight(pickedEnemyName);
-        }
-        // if player isn't alive, stop the game
-        else {
-            window.alert('You have lost your robot in battle! Game Over!');
-        }
+for (var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators!");
     }
 
-    startGame()
-
-    // function to end the entire game
-    var endGame = function() {
-        if (playerHealth > 0) {
-            window.alert("Great job, you've survived the game! You now have a score")
-        } else {
-            window.alert("You've lost your robot in battle.");
-        }
-    }
-
-    //Ask player if they'd like to play again
-    var playAgainConfirm = window.confirm("Would you like to play again?");
-
-    if (playAgainConfirm) {
-        //restart the game
-        startGame();
-    } else {
-        window.alert("Thank you for playing Robot Gladiators! Come back Soon!");
-    }
+    enemyHealth = 50;
+    fight(enemyNames[i]);
 };
+// run fight function to start game
+
+//fight();
+
+// Game States
+// Win Player Robot has defeated all enemy robots
+// fight all enemy robots
+// defeat each enemy robot
+// Lose player robots health is zero or less
